b6a96b91f922c6989dbafd8813479506125d16b2
Anna2804 <smileworld2804@gmail.com>
2019-10-15 20:37:54 +0300

    commited java files

---
+++ src/main/java/com/ivanova/App.java
@@ -0,0 +1,23 @@
+package com.ivanova;
+
+import java.util.*;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //Создаем массив случайных чисел
+        int arr[] = new int[10];
+        for(int i = 0; i <  arr.length; i++) {
+            arr[i] =  (int)(Math.random() * 100);
+            System.out.print(arr[i] + "  ");
+        }
+        System.out.print("\nSorted: \n");
+        //Сортируем массив
+        Arrays.sort(arr);
+        //Выводим отсортированный массив на консоль.
+        for(int i = 0; i <  arr.length; i++) {
+            System.out.print(arr[i] + "  ");
+        }
+    }
+}
